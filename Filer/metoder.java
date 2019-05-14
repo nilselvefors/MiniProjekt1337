b1b685a2ca1b751b0163cbc07c6399289c6575e9
xXxHacker BoixXx <nils.elvefors@hotmail.com>
2019-05-14 14:27:26 +0200

    Metod för att räkna ord

---
+++ Filer/metoder.java
@@ -0,0 +1,33 @@
+import java.text.*;
+public class metoder {
+	
+	public static String makeSpaceInString(String originalString){
+		
+		originalString = originalString.replaceAll("[\\{ \\} \\( \\) \\; \\= \\== \\< \\> \\+ \\- \\, \\[ \\] ]"," $0 ");
+		
+		System.out.println(originalString);
+		
+		return originalString;
+	}
+	
+	
+	public static int countWordsAndSymbols(String string) {
+		int counter = 0;
+		
+		CharacterIterator iterator = new StringCharacterIterator(string);
+		while(iterator.current() != CharacterIterator.DONE) {
+			counter++;
+			iterator.next();
+		}
+		return counter;
+		
+		
+	}
+	
+	public static void main(String[] args) {
+		String test = "for(int i=0;)";
+		String newString = makeSpaceInString(test);
+		//System.out.println(newString);
+		} 
+	}
+
