2ae9c3fd38ea1102d65b11e365f4dac0e1e73f7a
xXxHacker BoixXx <nils.elvefors@hotmail.com>
2019-05-14 15:35:50 +0200

    Började på main

--- s/src/s/mainProgram.java
+++ s/src/s/mainProgram.java
@@ -1,5 +1,33 @@
 package s;
 
-public class mainProgram {
+import java.io.FileNotFoundException;
 
+public class mainProgram {
+	
+	public static void main(String[] args) throws FileNotFoundException {
+		HashTabel javaNyckelordHasTabell = new HashTabel();
+		javaNyckelordHasTabell.readKeyWords();
+		
+		
+		String doc1OriginalString = metoder.readDoc("javakod.txt");
+		System.out.println(doc1OriginalString);
+		
+		String doc1SpaceString = metoder.makeSpaceInString(doc1OriginalString);
+		System.out.println(doc1SpaceString);
+		
+		String[]  doc1CleanTextArray = metoder.splitJavaCode(doc1OriginalString);
+		
+		
+		
+		
+		
+		
+		//System.out.println(doc1OriginalString);
+		
+		
+//		for(int i = 0; i < doc1CleanTextArray.length;i ++) {
+//			System.out.println(doc1CleanTextArray[i]);
+//		}
+		
+	}
 }
--- s/src/s/metoder.java
+++ s/src/s/metoder.java
@@ -1,14 +1,17 @@
 package s;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.text.CharacterIterator;
 import java.text.StringCharacterIterator;
+import java.util.Scanner;
 public class metoder {
 	
 	public static String makeSpaceInString(String originalString){
 		
 		originalString = originalString.replaceAll("[\\{ \\} \\( \\) \\; \\= \\== \\< \\> \\+ \\- \\, \\[ \\] ]"," $0 ");
 		
-		System.out.println(originalString);
+		
 		
 		return originalString;
 	}
@@ -25,6 +28,34 @@ public class metoder {
 		return counter;
 		
 		
+	}
+	public static String readDoc(String fileName) throws FileNotFoundException {
+		StringBuilder string = new StringBuilder();
+		Scanner scan = new Scanner(new File(fileName));
+		while (scan.hasNext()) {
+			string.append(scan.next().trim());
+			
+		}
+		return string.toString();
+		
+	}
+	
+	public static String removejavaKeyword(String javaString) {
+		
+		
+		
+		
+		
+		return "";
+	}
+	
+	
+	public static String [] splitJavaCode(String string) {
+		String [] split = string.trim().split("[\\{ \\} \\( \\) \\; \\= \\== \\< \\> \\+ \\- \\, \\[ \\] \\s+ ]+");
+//		for (String string2 : split) {
+//			System.out.println(string2);
+//		}
+		return split;
 	}
 	
 	public static void main(String[] args) {
