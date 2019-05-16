58652b87ca2eb834510d53c86037420c2023fbc1
Markus <mrrydeberg@gmail.com>
2019-05-16 21:54:02 +0200

    Some final changes

--- s/src/s/mainProgram.java
+++ s/src/s/mainProgram.java
@@ -1,9 +1,16 @@
 package s;
 
 import java.io.FileNotFoundException;
+
 import java.util.Iterator;
+import java.util.Scanner;
+import java.io.*;
 
 public class mainProgram {
+
+
+private static Scanner scan = new Scanner(System.in);
+
 	public static HashTabel ak(ReadDocument document) throws FileNotFoundException {
 		document.makeSpaceInString();
 		document.splitSpaces();
@@ -14,26 +21,22 @@ public class mainProgram {
 	}
 
 	public static void main(String[] args) throws FileNotFoundException {
-		
-		System.out.println("here is keyword in main");
-		System.out.println();
+//		metoder.choseTwoDocs();
+//		String doc1ChoiseString = scan.nextLine();
+//		System.out.println("-------------");
+//		String doc2ChoiseString = scan.nextLine();
+//		ReadDocument doc1 = new ReadDocument(doc1ChoiseString);
+//		ReadDocument doc2 = new ReadDocument(doc2ChoiseString);
 		ReadDocument doc1 = new ReadDocument("javakod.txt");
 		ReadDocument doc2 = new ReadDocument("javakod.txt");
 		HashTabel hash1 = ak(doc1);
 		HashTabel hash2 = ak(doc2);
 		
-		
 		System.out.println("-----------this is the fucking anser------------");
-		System.out.println(hash1.eval(hash2));
+//		metoder.theTwoComparedFilesResultat(doc1ChoiseString, doc2ChoiseString);
+		System.out.print(hash1.eval(hash2));
+		metoder.printTheAnswer();
 		System.out.println("-----------this is the fucking anser------------" +
 		"");
-		
-		
-		
-		
-		doc1.PrintOutStringAndArrays();
-		System.out.println("end stuff here");
-		hash1.printHashTable();
-		System.out.println(hash1.find("Nils"));
 	}
 }
