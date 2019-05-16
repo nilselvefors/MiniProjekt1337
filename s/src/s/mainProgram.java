a7b4ade9dc3cd934a7b1d1faf8bd757fae40ae61
Markus <mrrydeberg@gmail.com>
2019-05-16 13:50:50 +0200

    nils kom nu

--- s/src/s/mainProgram.java
+++ s/src/s/mainProgram.java
@@ -23,15 +23,5 @@ public class mainProgram {
 		doc1.PrintOutStringAndArrays();
 		System.out.println("end stuff here");
 		aHashTabel.printHashTable();
-		HashTabel hash1 = new HashTabel(50);
-		HashTabel hash2 = new HashTabel(50);
-		System.out.println("" + 
-		"");
-		hash1.addHash("string");
-		hash1.addHash("bok");
-		hash2.addHash("this is wrong");
-		hash1.printHashTable();
-		hash2.printHashTable();
-		System.out.println(hash1.find("bok"));
 	}
 }
