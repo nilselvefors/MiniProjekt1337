32fa6b499fda152647f5afc46153dfbb64a61a9f
Markus <mrrydeberg@gmail.com>
2019-05-24 10:44:42 +0200

    sum

--- s/src/s/tenta.java
+++ s/src/s/tenta.java
@@ -21,9 +21,37 @@ public class tenta {
 	}
 	public static void main(String[] args) {
 		System.out.println(ones(100));
-			
-		
-		
+		int [] arr = new int[10];
+		arr[0]=5;
+		arr[1]=3;
+		arr[2]=8;
+		arr[3]=1;
+		arr[4]=59;
+		arr[5]=512;
+		arr[6]=54;
+		arr[7]=5;
+		arr[8]=51;
+		arr[9]=55;
+		System.out.println(binarySearch(arr, 100));
 	}
+	public static int binarySearch( int [ ] a, int x )
+	 {
+	 // här kommer Tony att lägga till kod ( se uppgift d)
+
+	 int NOT_FOUND=-1;
+	 int low = 0;
+	 int high = a.length - 1;
+	 int mid;
+	 while( low <= high )
+	 {
+	 mid = ( low + high ) / 2;
+	 if ( x> a[ mid ] )
+	 low = mid + 1;
+	 else if(x< a[ mid ] )
+	 high = mid - 1;
+	 else
+	 return mid; }
+	 return NOT_FOUND;
+	 }
 
 }
